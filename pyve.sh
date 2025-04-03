718172b331e1cbdfdada98ea991af9bf6fdbf943
Michael Smith <pointmatic@gmail.com>
2025-04-03 16:53:33 -0700

    added support for pyenv as an alternative to asdf

--- pyve.sh
+++ pyve.sh
@@ -22,9 +22,9 @@
 #
 #   1. --init: Initialize the Python virtural environment
 #   Initializes Python environment with a sane setup
-#   - Runs asdf to set Python to version 3.11.11
+#   - Runs asdf (or if not installed, pyenv) to set Python to version 3.11.11
 #   - Runs venv to configure local environment for pip packages
-#     - Checks if .venv already exists in the current directory
+#     - Checks if .venv (or an provided dir name) already exists in the current directory
 #   - Configures direnv 
 #     - Checks if .envrc already exists in the current directory. 
 #     - If it exists, exit with a warning. 
@@ -45,11 +45,14 @@
 #   - Deletes the .env file
 
 # script version
-VERSION="0.1.2"
+VERSION="0.2.0"
 
 # asdf configuration
 PYTHONVERSION="3.11.11"
 ASDF_FILE_NAME=".tool-versions"
+USE_ASDF="true"
+PYENV_FILE_NAME=".python-version"
+USE_PYENV="false"
 DEFAULT_VENV_DIR_NAME=".venv" 
 #VENV_DIR_NAME is based on a parameter passed to the script, decided in init_config_dir_name()
 ENV_FILE_NAME=".env"
@@ -60,7 +63,7 @@ GITIGNORE_FILE_NAME=".gitignore"
 function show_help() {
     echo "\nHELP: Pyve.sh - Python Virtual Environment Setup Script\n"
     echo "Pyve will set up a special Python virtual environment in your current directory. "
-    echo "- auto-configure a version of Python (asdf)"
+    echo "- auto-configure a version of Python (asdf or pyenv)"
     echo "- autocreate a virtual environment (python venv)"
     echo "- autoactivate and deactivate the virtual environment when you change directory (direnv)"
     echo "- auto-configure an environment variable file .env (ready for dotenv package in Python)"
@@ -145,9 +148,17 @@ function purge() {
     rm -rf "$VENV_DIR_NAME"
     remove_pattern_from_gitignore "$VENV_DIR_NAME"
 
-    echo "\nDeleting asdf $ASDF_FILE_NAME file..."
-    rm -f "$ASDF_FILE_NAME"
-    remove_pattern_from_gitignore "$ASDF_FILE_NAME"
+    if [[ -f "$ASDF_FILE_NAME" ]]; then
+        echo "\nDeleting asdf $ASDF_FILE_NAME file..."
+        rm -rf "$ASDF_FILE_NAME"
+        remove_pattern_from_gitignore "$ASDF_FILE_NAME"
+    elif [[ -f "$PYENV_FILE_NAME" ]]; then
+        echo "\nDeleting pyenv $PYENV_FILE_NAME file..."
+        rm -rf "$PYENV_FILE_NAME"
+        remove_pattern_from_gitignore "$PYENV_FILE_NAME"
+    else
+        echo "\nHmmm... Neither asdf nor pyenv is configured here. No change."
+    fi
 
     echo "\nDeleting $DIRENV_FILE_NAME file..."
     rm -f "$DIRENV_FILE_NAME"
@@ -158,14 +169,6 @@ function purge() {
     remove_pattern_from_gitignore "$ENV_FILE_NAME"
 
     echo "\nAll artifacts of the Python virtual environment have been deleted.\n"
-}
-
-# not used
-function has_root_privileges() {
-    if [[ $EUID -ne 0 ]]; then
-        echo "\nThis script must be run as root. Please run it with sudo."
-        exit 1
-    fi
 }
 
 function init_ready() {
@@ -184,21 +187,38 @@ function init_ready() {
     fi
 
     # Check if asdf is installed
-    if ! command -v asdf &> /dev/null; then
-        echo "\nError: asdf is not installed. Please install asdf first."
+    if command -v asdf &> /dev/null; then
+        echo "\nasdf is installed."
+        USE_ASDF="true"
+    elif command -v pyenv &> /dev/null; then
+        echo "\npyenv is installed."
+        USE_PYENV="true"
+    else
+        echo "\nError: Neither asdf nor pyenv is installed. Please install one of them first."
+        echo "We need a Python version manager to set up the environment."
         exit 1
     fi
 
-    # Check if Python plugin is added in asdf
-    if ! asdf plugin list | grep -q "python"; then
-        echo "\nError: Python plugin is not added in asdf. Please add it first."
-        exit 1
-    fi
-
-    # Check if Python version is installed
-    if ! asdf list python | grep -q "$PYTHONVERSION"; then
-        echo "\nError: Python version $PYTHONVERSION is not installed in asdf. Please install it first."
-        exit 1
+    if [[ $USE_ASDF == "true" ]]; then
+        # Check if Python plugin is added in asdf
+        if ! asdf plugin list | grep -q "python"; then
+            echo "\nError: Python plugin is not added in asdf."
+            echo "Run: asdf add python"
+            exit 1
+        fi
+        # Check if Python version is installed
+        if ! asdf list python | grep -q "$PYTHONVERSION"; then
+            echo "\nError: Python version $PYTHONVERSION is not installed in asdf."
+            echo "Run: asdf install python $PYTHONVERSION"
+            exit 1
+        fi
+    elif [[ $USE_PYENV == "true" ]]; then
+        # Check if Python version is installed
+        if ! pyenv versions | grep -q "$PYTHONVERSION"; then
+            echo "\nError: Python version $PYTHONVERSION is not installed in pyenv."
+            echo "Run: pyenv install $PYTHONVERSION"
+            exit 1
+        fi
     fi
 
     # Check if direnv is installed
@@ -206,13 +226,14 @@ function init_ready() {
         echo "\nError: direnv is not installed. Please install direnv first."
         exit 1
     fi
+
+    return 0 # success
 }
 
 function init_dotenv() {
     # Check if .env file already exists
     if [[ -f "$ENV_FILE_NAME" ]]; then
         echo "\nDotenv file already exists! (found $ENV_FILE_NAME) \nNo change.\n"
-        #exit 1
     else
         # Create .env file and set permissions
         touch $ENV_FILE_NAME
@@ -247,17 +268,23 @@ function init_config_dir_name() {
         echo "\nError: The Venv directory name ($VENV_DIR_NAME) conflicts with the Git configuration file to ignore certain patterns.\nPlease provide another name." 
     elif [[ $VENV_DIR_NAME == $ASDF_FILE_NAME ]]; then
         echo "\nError: The Venv directory name ($VENV_DIR_NAME) conflicts with the asdf configuration file name.\nPlease provide another name." 
+    elif [[ $VENV_DIR_NAME == $PYENV_FILE_NAME ]]; then
+        echo "\nError: The Venv directory name ($VENV_DIR_NAME) conflicts with the pyenv configuration file name.\nPlease provide another name." 
     elif [[ $VENV_DIR_NAME == $DIRENV_FILE_NAME ]]; then
         echo "\nError: The Venv directory name ($VENV_DIR_NAME) conflicts with the Direnv configuration file name. Please provide another name."
-        exit 1
+    else
+        echo "\nGreat! The Venv directory name is valid: $VENV_DIR_NAME.\n"
+        return 0 # no conflict, continue
     fi
+
+    # If we reach this point, the directory name conflicts with one of the reserved names
+    exit 1 # error
 }
 
 function init_asdf() {
     # Check if asdf .tool-versions file exists
     if [[ -f "$ASDF_FILE_NAME" ]]; then
         echo "\nasdf has already been configured for this directory! (found $ASDF_FILE_NAME) \nNo change.\n"
-        #exit 1
     else
         echo "\nChecking for current Python version..."
         which python
@@ -269,11 +296,25 @@ function init_asdf() {
     fi
 }
 
+function init_pyenv() {
+    # Check if pyenv .python-version file exists
+    if [[ -f "$PYENV_FILE_NAME" ]]; then
+        echo "\npyenv has already been configured for this directory! (found $PYENV_FILE_NAME) \nNo change.\n"
+    else
+        echo "\nChecking for current Python version..."
+        which python
+        python --version
+        echo "\nConfiguring Python version..."
+        pyenv local "$PYTHONVERSION"
+        echo "\nVersion set now to Python $PYTHONVERSION.\nNOTE: this version won't be active until you run 'direnv allow' to activate the environment.\n"
+        append_pattern_to_gitignore "$PYENV_FILE_NAME"
+    fi
+}
+
 function init_venv() {
     # Configure Python virtual environment, but check first if .venv already exists
     if [[ -d "$VENV_DIR_NAME" ]]; then
         echo "\nPython virtual environment is already set up! (found ${VENV_DIR_NAME}) \nNo change.\n"
-        #exit 1
     else
         python -m venv "$VENV_DIR_NAME"
         append_pattern_to_gitignore "$VENV_DIR_NAME"
@@ -284,7 +325,6 @@ function init_direnv() {
     # Configure for direnv, but check first if .envrc already exists
     if [[ -f ".envrc" ]]; then
         echo "\nDirenv has already been configured for this directory! (found .envrc) \nNo change.\n"
-        #exit 1
     else
         echo "\nConfiguring direnv for automated virtual environment activation..."
         if [[ -d "$VENV_DIR_NAME" ]]; then
@@ -309,9 +349,14 @@ function init() {
     if init_ready; then
         init_config_dir_name "$@"
         init_dotenv
-        init_asdf
+        if [[ $USE_ASDF == "true" ]]; then
+            init_asdf
+        elif [[ $USE_PYENV == "true" ]]; then
+            init_pyenv
+        fi
         init_venv
         init_direnv
+        # no success message, since we need the user to pay attention to 'direnv allow'
     else
         echo "\nError: Failed to configure Python virtual environment."
         exit 1
