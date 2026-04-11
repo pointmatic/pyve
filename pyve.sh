7fb6365549957988e0602cefbf43aa75714d7213
Michael Smith <pointmatic@gmail.com>
2026-04-10 22:36:41 -0700

    v1.10.1 - Update Python version to latest, v3.14.4

--- pyve.sh
+++ pyve.sh
@@ -29,8 +29,8 @@ set -euo pipefail
 # Configuration
 #============================================================
 
-VERSION="1.10.0"
-DEFAULT_PYTHON_VERSION="3.14.3"
+VERSION="1.10.1"
+DEFAULT_PYTHON_VERSION="3.14.4"
 DEFAULT_VENV_DIR=".venv"
 ENV_FILE_NAME=".env"
 TESTENV_DIR_NAME="testenv"
